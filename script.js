dd21dbf1678c04d782bad62f832f142fd0a87161
MD TABISH FARHAN <150028841+tabishfarhan7@users.noreply.github.com>
2025-07-07 13:10:57 +0530

    added home page

---
+++ script.js
@@ -0,0 +1,11 @@
+
+  function toggleMenu() {
+    const menu = document.getElementById("mobileMenu");
+    menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+  }
+
+
+  function handleUpload() {
+    // Replace this with modal or file upload functionality later
+    alert("Upload prescription feature coming soon!");
+  }
